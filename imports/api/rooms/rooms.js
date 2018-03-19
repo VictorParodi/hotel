9b544b75e6b95193bc32f8695a75ba0966e4400d
Victor <victor.parodi.montero@gmail.com>
2018-03-19 00:52:12 -0500

    Appliying schemas to the collections

--- imports/api/rooms/rooms.js
+++ imports/api/rooms/rooms.js
@@ -1,5 +1,27 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+SimpleSchema.extendOptions(['autoform']);
 
 const Rooms = new Mongo.Collection('rooms');
+
+const RoomSchema = new SimpleSchema({
+  roomNumber: { type: Number },
+  checkIn: { type: Date },
+  checkOut: { type: Date },
+  tenantID: { type: String },
+  available: { type: Boolean },
+  needCleaning: { type: Boolean },
+  createdAt: {
+    type: Date,
+    autoform: {
+      type: 'hidden',
+      label: false
+    },
+    defautlValue: new Date()
+  }
+});
+
+Rooms.attachSchema(RoomSchema);
 
 export default Rooms;
--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import Members from './../imports/api/members/members';
+import Rooms from './../imports/api/rooms/rooms';
 
 Meteor.startup(() => {
   // code to run on server at startup
