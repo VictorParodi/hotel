c20932f8b4b9d604c3ce6e6022aeb91abe5cf194
Victor <victor.parodi.montero@gmail.com>
2018-03-23 23:49:30 -0500

    Working with templates and helpers

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,16 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import{ Members } from './../api/members/members';
+import{ Rooms } from './../api/rooms/rooms';
 
 import './body.html';
 import './members.html';
+import './rooms.html';
 
 Template.body.onCreated(function bodyOnCreated() {
   Meteor.subscribe('members.allMembers');
+  Meteor.subscribe('rooms.allRooms');
 });
 
 Template.members.helpers({
   members() {
     return Members.find();
+  }
+});
+
+Template.rooms.helpers({
+  rooms() {
+    return Rooms.find();
   }
 });
